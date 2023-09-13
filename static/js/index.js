257e5f90a8a6cb2d0f897991481d54bdc1a9fef0
Lyubov Trifonova <valeryateplova000@gmail.com>
2023-09-13 21:49:30 +0300

    add: matrix

--- static/js/index.js
+++ static/js/index.js
@@ -7,6 +7,31 @@ if(itemNodes.length !== 16){
 }
 
 /** Position */
+let matrix = getMatrix( 
+    itemNodes.map((item) => Number(item.dataset.matrixId))
+)
+
+console.log(matrix);
+
 /** Shuffle */
 /** Change position by click*/
 /** Change position by keydown */
+
+/**Helpers */
+
+function getMatrix(arr){
+    const matrix = [[],[],[],[]];
+
+    let y = 0;
+    let x = 0;
+
+    for (let i=0; i<arr.length; i++){
+        if (x>=4){
+            y++;
+            x=0;
+        }
+        matrix[y][x] = arr[i];
+        x++;
+    }
+    return matrix;
+}
