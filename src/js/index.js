c044dffdadd3e812f82871f1042e12753d51d8b6
Lyubov Trifonova <valeryateplova000@gmail.com>
2023-09-14 00:37:11 +0300

    edit: js, add: swap

--- src/js/index.js
+++ src/js/index.js
@@ -30,13 +30,12 @@ containerNode.addEventListener('click', (event) => {
 
     const butonNumber = Number(buttonNode.dataset.matrixId);
     const buttonCoords = findCoordinatesByNumber(butonNumber, matrix);
-    const blankCoords = findCoordinatesByNumber(butonNumber, matrix);
+    const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
     const isValid = isValidForSwap(buttonCoords, blankCoords);
 
-    console.log(isValid);
-
     if (isValid){
-
+        swap(blankCoords, buttonCoords, matrix);
+        setPositionItems(matrix);
     }
 })
 /** Change position by keydown */
@@ -98,5 +97,11 @@ function findCoordinatesByNumber(number, matrix) {
 function isValidForSwap(coords1, coords2) {
     const diffX = Math.abs(coords1.x - coords2.x);
     const diffY = Math.abs(coords1.y - coords2.y);
-    return (diffX === 1 || diffY === 1);
+    return (diffX === 1 || diffY === 1) && (coords1.x === coords2.x || coords1.y === coords2.y);
+}
+
+function swap(coords1, coords2, matrix) {
+    const coords1Number = matrix[coords1.y][coords1.x];
+    matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x];
+    matrix[coords2.y][coords2.x] = coords1Number;
 }
