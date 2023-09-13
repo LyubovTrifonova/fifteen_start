8b257d368bdd36ddf04a046c5f1ada251231d48b
Lyubov Trifonova <valeryateplova000@gmail.com>
2023-09-13 21:58:21 +0300

    matrix

--- src/js/index.js
+++ src/js/index.js
@@ -36,3 +36,17 @@ function getMatrix(arr){
     return matrix;
 }
 
+function setPositionItems(matrix){
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            const value = matrix[y][x];
+            const node = itemNodes[value - 1];
+            setNodesStyles(node, x, y);
+        }
+    }
+}
+
+function setNodesStyles(node, x, y){
+    const shiftPs = 100;
+    node.style.transform = `translate3D(${x*shiftPs}%, ${y*shiftPs}%, 0)`
+}
